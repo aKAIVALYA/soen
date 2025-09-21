5cdc5d7674fe5ad2fe98d0ed9a92d11a4682ad09
kaivalya <kaivalyaadgulwar@gmail.com>
2025-09-21 19:36:30 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -7,30 +7,31 @@ import aiRoutes from './routes/ai.routes.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-connect(); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+connect();
 const app = express();
 
-
 app.use(cors({
-    orign:'http://localhost:5173',
-    cardinals: true
-}
-));
-app.use(morgan('dev')); 
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-
 app.use('/users', userRoutes);
 app.use('/projects', projectRoutes);
-app.use('/ai',aiRoutes)
+app.use('/ai', aiRoutes);
 
-
-app.get('*name', (req, res) =>{
+app.get('*name', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
-})
+});
 
 export default app;
